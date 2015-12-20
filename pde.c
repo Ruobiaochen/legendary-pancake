698c9a7f86992b96af20dd0288008cee4b1dd2d1
coconut32 <coconut_liu@126.com>
2015-12-20 09:25:24 +0800

    Create pde.c

---
+++ pde.c
@@ -0,0 +1,9 @@
+//gradient and directional derivative:consider the tangent space at the point
+//2 variable:integrate "along" gradients using small plane instead of short line as in one variable.
+//partial derivative:fix other variables and integrate as if in one variable.
+
+//strauss
+
+//ch1
+order:highest derivative that appears
+linear operator.
