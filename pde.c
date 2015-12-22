1a75d235464c3443021ea83e8318e2d8806da3ac
coconut32 <coconut_liu@126.com>
2015-12-22 13:00:43 +0800

    Update pde.c

--- pde.c
+++ pde.c
@@ -1,3 +1,6 @@
+#notational convention
+n is the normal vector pointed outward the region.
+
 //gradient and directional derivative:consider the tangent space at the point
 //2 variable:integrate "along" gradients using small plane instead of short line as in one variable.
 //partial derivative:fix other variables and integrate as if in one variable.
@@ -12,3 +15,13 @@ remark.the parametrization method (in D. Bleecker) seems overly complicated for 
 characteristic curve method.
 initial condition:specify u at all space points at time t0.
 boundary condition:specify u at all time at boundary of space points.
+conditions at infinity.
+jump conditions.
+well-posed:exist,unique,stable.
+eg.backward diffusion.singular matrix.half plane.
+types of second order equation.
+elliptic:positive-definite or negative-definite.
+hyperbolic:1+ other- or 1- other+.
+ultrahyperbolic.
+parabolic:1zero other same.
+if the coefficients are variable,the equation can change type in different regions.
