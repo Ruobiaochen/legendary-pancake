b66628b06a2e520bb2443b7a7dafba2b6780744c
coconut32 <coconut_liu@126.com>
2015-12-22 14:26:00 +0800

    Update pde.c

--- pde.c
+++ pde.c
@@ -10,8 +10,8 @@ n is the normal vector pointed outward the region.
 //ch1
 order:highest derivative that appears
 linear operator.
-remark.variable coefficient equation can't be reduced to a change of coordinate between partial derivatives(thus the change of x,y,z,...) because this will induce to solve a similar equation.
-remark.the parametrization method (in D. Bleecker) seems overly complicated for simple computational problems.
+remark.(maybe locally,but I don't know) variable coefficient equation can't be reduced to a change of coordinate between partial derivatives(thus the change of x,y,z,...) because this will induce to solve a similar equation.
+remark.the parametrization method (in D. Bleecker) is overly complicated for simple computational problems.
 characteristic curve method.
 initial condition:specify u at all space points at time t0.
 boundary condition:specify u at all time at boundary of space points.
