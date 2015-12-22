2ff6ad8266bd9f361240d2a5e2866d4dd6d04a88
coconut32 <coconut_liu@126.com>
2015-12-22 11:57:48 +0800

    Update pde.c

--- pde.c
+++ pde.c
@@ -10,3 +10,5 @@ linear operator.
 remark.variable coefficient equation can't be reduced to a change of coordinate between partial derivatives(thus the change of x,y,z,...) because this will induce to solve a similar equation.
 remark.the parametrization method (in D. Bleecker) seems overly complicated for simple computational problems.
 characteristic curve method.
+initial condition:specify u at all space points at time t0.
+boundary condition:specify u at all time at boundary of space points.
