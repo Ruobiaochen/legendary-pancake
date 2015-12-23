c599753cd31f1538952f069c9cba0cb3470d4b20
coconut32 <coconut_liu@126.com>
2015-12-23 22:17:14 +0800

    Update pde.c

--- pde.c
+++ pde.c
@@ -10,8 +10,8 @@ n is the normal vector pointed outward the region.
 //ch1.intro
 order:highest derivative that appears
 linear operator.
-remark.(maybe locally,but I don't know) variable coefficient equation can't be reduced to a change of coordinate between partial derivatives(thus the change of x,y,z,...) because this will induce to solve a similar equation.
-remark.the parametrization method (in D. Bleecker) is overly complicated for simple computational problems.
+//remark.(maybe locally,but I don't know) variable coefficient equation can't be reduced to a change of coordinate between partial derivatives(thus the change of x,y,z,...) because this will induce to solve a similar equation.
+//remark.the parametrization method (in D. Bleecker) is overly complicated for simple computational problems.
 characteristic curve method.
 initial condition:specify u at all space points at time t0.
 boundary condition:specify u at all time at boundary of space points.
@@ -20,7 +20,7 @@ jump conditions.
 well-posed:exist,unique,stable.
 eg.backward diffusion.singular matrix.half plane.
 types of second order equation.
-remark.in the classification,we rename t to y.
+//remark.in the classification,we rename t to y.
 elliptic:positive-definite or negative-definite.
 hyperbolic:1+ other- or 1- other+.
 ultrahyperbolic.
@@ -44,7 +44,7 @@ Stability:two sense
 ut=kuxx,x in R,t>0,k is not 0.u(x,0)=p(x).
 many invariance property.
 fundamental solution:S(x,t)=1/sqrt(4pi*kt)*e^(-x^2/(4kt))
-general solution,ignoring t:u(x)=H(S,p)(x)=int -inf to inf S(x-k)p(k)dk.
+general solution,ignoring t:u(x)=H(S,p)(x)=int -inf to inf S(x-k)p(k)dk.in operator form:u=Ap,where A is called the source operator.
 
 //ch3.reflections and source
 ut=kuxx,x>0,t>0,k not 0.u(x,0)=p(x),u(0,t)=0.
@@ -57,3 +57,5 @@ wave on finite interval,extension method.
 ut-kuxx=f(x,t),x in R,t>0.u(x,0)=p(x).
 Duhamel's principle method.
 consider ut-kuxx=f(x,t),x in R,t>0.u(x,0)=0. and ut-kuxx=0,x in R,t>0.u(x,0)=p(x). and combine to get the solution.
+the integral factor is A(-t),where A is source operator mentioned above,so that u-kuxx=u-Bu=d/dt(A(-t)u(t)).(B is a temporary notation)
+//remark.what is A(-t)? I don't know now...(A is well-defined for A(t) s.t. t>0.)
