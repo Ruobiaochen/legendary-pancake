0e345f4585b995b709e3af3c8715b87cb8dad3ad
coconut32 <coconut_liu@126.com>
2016-01-09 00:47:19 +0800

    Create pde-restatement.c

---
+++ pde-restatement.c
@@ -0,0 +1,15 @@
+//A restatement of notes in pde.c.
+
+general
+Duhamel's principle method.
+subtraction method(shifting the data).
+separated solution method.
+fourier transform method.
+
+linear equation:first order
+characteristic curve method.
+parametrization method.
+
+linear equation:second order:wave
+utt=c^2*uxx,in the domain R*R,solution:u(x,t)=f(x+ct)+g(x-ct).
+reflection(extension) method.
