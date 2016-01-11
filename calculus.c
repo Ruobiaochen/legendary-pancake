571fdf5597c718bc0310622ff0b77870e0a4fc87
coconut32 <coconut_liu@126.com>
2016-01-11 15:58:29 +0800

    Create calculus.c

---
+++ calculus.c
@@ -0,0 +1,11 @@
+(this is for preparing the test so not rigorous)
+
+//general
+Cauchy's definition of limit convergence.
+sup calculation.
+
+//improper integral
+improper integral:int a to +inf f(x)dx converges means lim A->+inf int a to A f(x)dx exists.
+limit method for positive f(x).
+lim f(x) need not be zero.(but lim inf |f(x)| should.)
+absolute convergence for improper integral.
