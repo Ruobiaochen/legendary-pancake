5bc5a3b919d3d200bdbe4a02ede2b828f9a02079
coconut32 <coconut_liu@126.com>
2016-01-14 19:52:50 +0800

    Update linear algebra.c

--- linear algebra.c
+++ linear algebra.c
@@ -3,8 +3,10 @@ Frobenius normal form(rational canonical form):maximal decomposition into cyclic
 generalized Jordan normal form(primary rational canonical form):minimal decomposition
 Smith normal form of the eigenmatrix corresponds to normal forms of the original matrix.
 
-//Quadratic form,inner product,bilinear form and conjugate bilinear form
+//Quadratic form,inner product,bilinear form(and sesquilinear form),dual space
 quadratic form corresponds to symmetric matrix.
 norm can induce inner product. that is,the form XtQX and XtQY can express each other.
+a (nondegenerate) bilinear form corresponds to an isomorphism between V and V*.(cf. bilinear mapping)
+
 
 //Inner product space
