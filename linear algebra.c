f2bb4819e0c945b5fe6d384ca87eca0c6da4a7c9
coconut32 <coconut_liu@126.com>
2016-01-14 16:58:37 +0800

    Update linear algebra.c

--- linear algebra.c
+++ linear algebra.c
@@ -2,3 +2,5 @@
 Frobenius normal form(rational canonical form):maximal decomposition into cyclic(stable) subspace
 generalized Jordan normal form(primary rational canonical form):minimal decomposition
 Smith normal form of the eigenmatrix corresponds to normal forms of the original matrix.
+
+//Quadratic form
