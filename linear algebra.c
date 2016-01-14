ac4285f76aaf8a138ce0efc03e765f96829555cf
coconut32 <coconut_liu@126.com>
2016-01-14 12:57:15 +0800

    Create linear algebra.c

---
+++ linear algebra.c
@@ -0,0 +1,3 @@
+//Similarity
+Frobenius normal form(rational canonical form):maximal decomposition into cyclic(stable) subspace
+generalized Jordan normal form(primary rational canonical form):minimal decomposition
