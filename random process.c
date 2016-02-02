aa2d68e26ca3b54f8b76551be87ca62cd68a4eaf
coconut32 <coconut_liu@126.com>
2016-02-02 11:00:27 +0800

    Create random process.c

---
+++ random process.c
@@ -0,0 +1,10 @@
+//For Kalman filter.
+
+random variable: a measurable function X:O->E,where O is a probability space,E a measurable space.
+remark.it's a function from o in O (outcome) to something depend on it.
+probability space: a triple {O,F,P}
+remark. O is sample space (outcomes),F is event sets,P is measure.
+expected value: E[g(x)],abbreviated to Eg(x).
+discrete random(stochastic) process: a sequence of random variables x(t).
+realization(sample function) of the process:theta(t),which is one particular set of observed values of x(t).
+uncorrelated random process: t!=s -> Ex(t)x(s)=Ex(t)Ex(s)
