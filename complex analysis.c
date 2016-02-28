0a4c0a2d53d361235409b201e2d7bb0a017bb170
coconut32 <coconut_liu@126.com>
2016-02-28 20:03:26 +0800

    Update complex analysis.c

--- complex analysis.c
+++ complex analysis.c
@@ -1,2 +1,3 @@
 //As with the (locally) approximation by linear map,here C->C is approached by y(z)=az,which means amplifying and rotating.
 //duality:conjugate
+//take care of a complex number equals zero:re part and im part both zero.
