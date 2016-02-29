e2134821ba34afe0f6f4d86d447b1f65a06ed354
coconut32 <coconut_liu@126.com>
2016-02-29 22:59:31 +0800

    Update complex analysis.c

--- complex analysis.c
+++ complex analysis.c
@@ -1,3 +1,9 @@
 //As with the (locally) approximation by linear map,here C->C is approached by y(z)=az,which means amplifying and rotating.
 //duality:conjugate
 //take care of a complex number equals zero:re part and im part both zero.
+
+//ch1.preliminary
+Arg is a set,Arg0 is undefined,arg is between (-pi,pi].
+point at infinity:one point compactification.
+Riemann sphere.
+//remark.this can remove some kind of singularities by set them to the infinity point.
