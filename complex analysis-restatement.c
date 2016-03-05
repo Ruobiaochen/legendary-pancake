368285f4b957aaa2bc89efe1ef40a8fa511620fa
coconut32 <coconut_liu@126.com>
2016-03-06 01:25:25 +0800

    Update complex analysis-restatement.c

--- complex analysis-restatement.c
+++ complex analysis-restatement.c
@@ -9,10 +9,15 @@ in a circle,holomorphic is equivalent to analytic
 estimation of derivative
 Cauchy inequality
 
-m-th zero
-lemma.relationship between factorization and derivative
+fiber (ie. zero)
+lemma.relationship between factorization and derivative and series
 isolated zero theorem
 lemma.analytic(in the sense of opposite to topological) representation of winding number
 argument principle
 Rouche theorem
+local invariant of zero number
+Hurwitz theorem for function limit invariant of zero number
 
+open mapping theorem
+
+injective implies non-vanishing derivative implies biholomorphic
