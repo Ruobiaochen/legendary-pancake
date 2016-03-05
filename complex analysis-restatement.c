0d40bb5dfc1009454c30a40995ccb919b440a62d
coconut32 <coconut_liu@126.com>
2016-03-05 22:21:31 +0800

    Create complex analysis-restatement.c

---
+++ complex analysis-restatement.c
@@ -0,0 +1,18 @@
+main object:f:C->C
+
+fundamental lemma
+complex differentiable equals Cauchy-Riemann equation
+Cauchy solution to Neumann problem of C-R
+Cauchy theorem for " " holomorphic
+in a circle,holomorphic is equivalent to analytic
+
+estimation of derivative
+Cauchy inequality
+
+m-th zero
+lemma.relationship between factorization and derivative
+isolated zero theorem
+lemma.analytic(in the sense of opposite to topological) representation of winding number
+argument principle
+Rouche theorem
+
