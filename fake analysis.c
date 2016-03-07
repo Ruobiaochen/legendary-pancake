5255046ce1b8de9359bdeeff8eb2169f34400937
coconut32 <coconut_liu@126.com>
2016-03-08 01:07:01 +0800

    Update fake analysis.c

--- fake analysis.c
+++ fake analysis.c
@@ -22,3 +22,16 @@ Ek go up to E:union(Ek)=E and Ek is in Ek+1.
 Ek go down to E:intersection(Ek)=E and Ek+1 is in Ek.
 //if Ek go up to E,m(E)=lim m(Ek).
 //if Ek go down to E and m(Ek) is finite,m(E)=lim m(Ek).
+//A Lebesgue-measurable set can be "squeezed" between a containing open set and a contained closed set.
+//Lebesgue measure is inner regular:the measure of a set can be approximated from within by compact subsets.
+//approximation by finite union of closed cubes with symmetric difference.
+//invariance property under translation and dilation
+sigma-algebra:a sigma-algebra on a set X is a collection of subsets of X that is closed under complement,countable union,countable intersection.
+Borel sigma-algebra:the intersection of all sigma-algebra on Rn that contains all open sets
+Borel set:element of Borel sigma-algebra
+//there exists Lebesgue measurable set which is not Borel set.
+G-delta set:a set which is countable intersection of open sets
+F-sigma set:a set which is countable union of closed sets
+null set.
+//A Lebesgue-measurable set can be "squeezed" between a containing Gδ set and a contained Fσ set.
+
