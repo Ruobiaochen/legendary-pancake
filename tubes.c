142639f8c8452ee01e4ef115c0115ce4c3e20693
coconut32 <coconut_liu@126.com>
2016-04-28 14:34:35 +0800

    Create tubes.c

---
+++ tubes.c
@@ -0,0 +1,11 @@
+------------------------------------------------------------------------------------------------
+inequality and extremum
+
+
+------------------------------------------------------------------------------------------------
+limit and extremum(eg.rou(delta)=sup(...<delta) & lim rou->0,which admits a yet another reformation because of monotonicity of rou:inf(rou)=0,ie.inf(sup())=0)
+
+convex and extremum(eg.Hadamard three circle theorem)(cf.inequality and extremum)
+convex and continuity
+
+cos and e
