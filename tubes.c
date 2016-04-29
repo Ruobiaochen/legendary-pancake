a172498f780447305122b6afffbf655f21ec8763
coconut32 <coconut_liu@126.com>
2016-04-29 10:26:30 -0500

    Update tubes.c

--- tubes.c
+++ tubes.c
@@ -4,7 +4,7 @@ complexification:1.real function as a complex function and 2.its holomorphiction
 
 
 ------------------------------------------------------------------------------------------------
-limit and extremum(eg.rou(delta)=sup(...<delta) & lim rou->0,which admits a yet another reformation because of monotonicity of rou:inf(rou)=0,ie.inf(sup())=0)
+limit and extremum(eg.rou(delta)=sup(...<delta) & lim rou->0,which admits a yet another reformation because of monotonicity of rou:inf(rou)=0,ie.inf(sup())=0)(cf.inequality and extremum)
 
 convex and extremum(eg.Hadamard three circle theorem)(cf.inequality and extremum)
 convex and continuity
