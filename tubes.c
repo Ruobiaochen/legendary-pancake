2552fdccf0ca0a9efae97f6d72f617ccdc8e972a
coconut32 <coconut_liu@126.com>
2016-05-12 01:43:15 +0800

    Update tubes.c

--- tubes.c
+++ tubes.c
@@ -1,18 +1,20 @@
 ------------------------------------------------------------------------------------------------
 boundary information
+complexification:1.real function as a complex function and 2.its holomorphiction(by harmonic conjugate pair)(eg.cos and e,fourier transform and laplace transform)
+complex analysis and fourier analysis
+energy and integral and area/volume
 
 
 ------------------------------------------------------------------------------------------------
 inequality and extremum(cf.boundary information)
+zero and equality(eg. homo and kernel,invariance zero theorem(note that here zero means lingdian,something totally different,but the zero and equality principle is used in the proof))(cf.boundary information)
 zero inside a zone and information on the boundary(eg.argument principle,Rouche theorem)(cf.boundary information)
 
-complexification:1.real function as a complex function and 2.its holomorphiction(by harmonic conjugate pair)
-
+Cauchy integral formula and fourier coefficients(cf.complex analysis and fourier analysis)
+Gronwall area theorem and Parseval equation(cf.complex analysis and fourier analysis)(cf.energy and integral and area/volume)
 
 ------------------------------------------------------------------------------------------------
 limit and extremum(eg.rou(delta)=sup(...<delta) & lim rou->0,which admits a yet another reformation because of monotonicity of rou:inf(rou)=0,ie.inf(sup())=0)(cf.inequality and extremum)
 
 convex and extremum(eg.Hadamard three circle theorem)(cf.inequality and extremum)
 convex and continuity
-
-cos and e(cf.complexification)
