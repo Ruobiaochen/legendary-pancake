c2eebcf983a0dd00748fe884886fd8fb7942290d
coconut32 <coconut_liu@126.com>
2016-05-23 14:23:24 +0800

    Update assemble.c

--- assemble.c
+++ assemble.c
@@ -1,6 +1,9 @@
-This is meant to be an assemble of step-by-step methods.
+This is meant to be an assemble of standard methods.
 
 //series convergence/divergence
 1.tends to zero?
 2.absolute convergence?
 3.conditional convergence?
+
+//matrix inverse
+use row or column operation,but cannot use both together.
