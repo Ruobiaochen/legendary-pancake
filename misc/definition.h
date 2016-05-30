1fc42aea0743a5a7673e118ecc6e5180b1b74b42
coconut32 <coconut_liu@126.com>
2016-05-30 13:40:51 +0800

    Update definition.h

--- misc/definition.h
+++ misc/definition.h
@@ -1,2 +1,3 @@
 interval subset of A:subset of set A in R,in the form of the union of finitely many disjoint subintervals A1,..,An(n is not fixed)
 absolutely continuous:f:[a,b]->R,st. inf_d sup_(E=interval subset of [a,b],st.m(E)<d) sigma(|f(bk)-f(ak)|) = 0,where ak and bk are endpoints of subintervals in E
+Cantor-Lebesgue function:a map from Cantor set to [0,1]. Can also be extended to [0,1]->[0,1].
