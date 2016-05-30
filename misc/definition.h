a825750ad1b93e44863d7c72e472b1af1a9d37de
coconut32 <coconut_liu@126.com>
2016-05-30 13:31:21 +0800

    Create definition.h

---
+++ misc/definition.h
@@ -0,0 +1,2 @@
+interval subset of A:subset of set A in R,in the form of the union of finitely many disjoint subintervals A1,..,An(n is not fixed)
+absolutely continuous:f:[a,b]->R,st. inf_d sup_(E=interval subset of [a,b],st.m(E)<d) sigma(|f(bk)-f(ak)|) = 0,where ak and bk are endpoints of subintervals in E
