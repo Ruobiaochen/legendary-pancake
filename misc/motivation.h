6d01afa00ab6560033645236f2743b110e9bb4f8
coconut32 <coconut_liu@126.com>
2016-09-06 22:22:01 +0800

    Update motivation.h

--- misc/motivation.h
+++ misc/motivation.h
@@ -1,2 +1,3 @@
 
 metric space: there is a need to describe different mode of convergence, and metric space with different metric function can induce different topology, which does the work.
+complete: this is a condition in Banach fixed point theorem.
