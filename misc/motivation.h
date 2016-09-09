c4ae97bce40bf4685be5962383b3159258750033
coconut32 <coconut_liu@126.com>
2016-09-10 01:01:42 +0800

    Update motivation.h

--- misc/motivation.h
+++ misc/motivation.h
@@ -1,3 +1,4 @@
 
 metric space: there is a need to describe different mode of convergence, and metric space with different metric function can induce different topology, which does the work.
 complete: this is a condition in Banach fixed point theorem.
+relatively compact: condition in Arzela-Ascoli theorem
