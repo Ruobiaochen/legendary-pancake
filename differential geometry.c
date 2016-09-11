9d510cc454a6e3a1b58f027abaff29cb94ab5ff2
coconut32 <coconut_liu@126.com>
2016-09-12 00:33:15 +0800

    Create differential geometry.c

---
+++ differential geometry.c
@@ -0,0 +1,7 @@
+Overall important idea:
+coordinate free formulas
+
+Keys in 3-d smooth curve theory:
+natural (ie. arc) parametrization
+Frenet frame & F-S formula
+configuration space is curvature & torsion
