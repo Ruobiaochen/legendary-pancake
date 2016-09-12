778ef46fcccfd42edf09ca9b317bc180661900b3
coconut32 <coconut_liu@126.com>
2016-09-12 16:06:47 +0800

    Update motivation.h

--- misc/motivation.h
+++ misc/motivation.h
@@ -6,3 +6,6 @@ relatively compact: condition in Arzela-Ascoli theorem
 
 regular (curve): condition so that arc parameter is well-defined
 signed (curvature on plane): since in the plane two vectors are enough to define an orientation, so we can define the normal vector to keep the orientation as right-handed.
+
+
+topology space structure (ie. specification of open subsets): to describe such concepts as continuity, connectedness.
