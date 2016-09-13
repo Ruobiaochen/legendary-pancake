45d96bd1a614d3c6a09c1380cc378787ecae7bd0
coconut32 <coconut_liu@126.com>
2016-09-13 12:23:41 +0800

    Update motivation.h

--- misc/motivation.h
+++ misc/motivation.h
@@ -1,12 +1,17 @@
 
+(FA)
 metric space: there is a need to describe different mode of convergence, and metric space with different metric function can induce different topology, which does the work.
 complete: this is a condition in Banach fixed point theorem.
-relatively compact: condition in Arzela-Ascoli theorem
+relatively compact: condition in Arzela-Ascoli theorem 
+epsilon net: used in the definition of totally bounded
+totally bounded: equivalent to relatively compact in a complete metric space
 
 
+(DG)
 regular (curve): condition so that arc parameter is well-defined
 signed (curvature on plane): since in the plane two vectors are enough to define an orientation, so we can define the normal vector to keep the orientation as right-handed.
 
 
+(DM)
 topology space structure (ie. specification of open subsets): to describe such concepts as continuity, connectedness.
 (topological) manifold: to describe locally n-dimensional objects, in the sense of Euclidean topology
