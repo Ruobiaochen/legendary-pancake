5e686fc363e8ec613adbac47352f7a12b89ded14
coconut32 <coconut_liu@126.com>
2016-09-19 19:03:33 +0800

    Update differential geometry.c

--- differential geometry.c
+++ differential geometry.c
@@ -1,7 +1,7 @@
 Overall important idea:
 coordinate free formulas
 parametrization free formulas
-moving frame formulas (local intrinsic invariant formula)
+moving frame representation/diff equation (local intrinsic invariant)
 
 Keys in 3-d smooth curve theory:
 natural (ie. arc) parametrization
