9af510f7116c27178db74cf36d7b464105c6d301
coconut32 <coconut_liu@126.com>
2016-09-19 21:18:58 +0800

    Update differential geometry.c

--- differential geometry.c
+++ differential geometry.c
@@ -2,8 +2,8 @@ Overall important idea:
 coordinate free formulas
 parametrization free formulas
 moving frame representation/diff equation (local intrinsic invariant)
+simplification by reparametrization
 
 Keys in 3-d smooth curve theory:
-natural (ie. arc) parametrization
 Frenet frame & F-S formula
 configuration space is curvature & torsion
