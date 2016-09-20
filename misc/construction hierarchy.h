59da28f583e98685810763bf8cf681397ed1919b
coconut32 <coconut_liu@126.com>
2016-09-21 01:00:19 +0800

    Update construction hierarchy.h

--- misc/construction hierarchy.h
+++ misc/construction hierarchy.h
@@ -2,3 +2,11 @@
 0.2 (in R) closed and bounded set in R always has a minimum
 
 1.1 (in topological space) f: compact M->R can attain its minimum (0.2)
+
+1i.1 smoothly zero function: R->R, 0 when x<0, else e^(-1/x).
+
+2i.1 smooth transition function: R->R, \cite(1i.1) as h, h(x)/(h(x)+h(1-x)).
+
+3i.1 bump function: Rn->R, \cite(2i.1) as g, g(2-|x|).
+
+4i.1 bump function: manifold M->R, \cite(3i.1) as p, \cite(2i.1) as q, q(sigma(p_i(phi_i)))
