924672f7fecc95bff726b6cabbcf26526973618e
coconut32 <coconut_liu@126.com>
2016-09-21 09:34:27 +0800

    Update construction hierarchy.h

--- misc/construction hierarchy.h
+++ misc/construction hierarchy.h
@@ -5,14 +5,16 @@
 
 1i.1 smoothly zero function: R->R, 0 when x<0, else e^(-1/x).
 1i.2 contraction mapping (Banach fixed point)
+1i.3 a topological existence lemma for partition of unity
 
 2i.1 smooth transition function: R->R, \cite(1i.1) as h, h(x)/(h(x)+h(1-x)).
-2i.2 inverse function theorem (1i.2)
+2i.2 inverse function taheorem (1i.2)
 
 3i.1 bump function: Rn->R, \cite(2i.1) as g, g(2-|x|).
 
 4i.1 bump function: manifold M->R, \cite(3i.1) as p, \cite(2i.1) as q, q(sigma(p_i(phi_i)))
 
-5i.1 partition of unity subordinate to open cover U_a: \cite(4i.1) as phi_j, rou_a=sigma(phi_j/sigma(phi_j))
+5i.1 partition of unity subordinate to open cover U_a and subordinate to compact subsets of U_a: rou_a,rou_j: manifold M->R, \cite(4i.1) as phi_j, rou_a=sigma(rou_j), where rou_j=phi_j/sigma(phi_j) (1i.3)
 
 6i.1 bump function for closed set A: use partition of unity for {M/A,U} (5i.1)
+6i.2 existence of exhaustion function: \cite(5i.1) as {rou_a,rou_j}, sigma(j*rou_j)
