a0bfb3e92fa1c2c6fb9492112b8ddad0fc2d675e
coconut32 <coconut_liu@126.com>
2016-11-20 23:35:37 +0800

    Update differential geometry.c

--- differential geometry.c
+++ differential geometry.c
@@ -7,3 +7,10 @@ simplification by reparametrization
 Keys in 3-d smooth curve theory:
 Frenet frame & F-S formula
 configuration space is curvature & torsion
+
+Keys in 3-d smooth surface theory:
+Intrinsic quantity: Gauss curvature
+Gauss map and its derivative: W transformation, ie. shape operator
+orthogonal parametrization, isothermal parametrization
+motion equation and its integrable condition(Gauss-Codazzi)
+configuration space is two fundamental forms which satisfy Gauss-Codazzi
